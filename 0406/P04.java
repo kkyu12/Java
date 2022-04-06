82ddf89710771b1e16145073ee2f173140742a29
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-04-06 19:02:21 +0900

    for-each loop Example

---
+++ 0406/P04.java
@@ -0,0 +1,13 @@
+package Apr6th;
+
+public class P04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] data = {"abc","de","fghi","j","kl"};
+		for (String d : data) {
+			System.out.println(d);
+		}
+	}
+
+}
