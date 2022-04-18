cb17b494340aeaed6d9811e08bdd8d1930f8290d
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-04-18 17:58:36 +0900

    띄어쓰기 연습

---
+++ 0415_04/Practice12.java
@@ -0,0 +1,17 @@
+package Apr15th_04;
+
+public class Practice12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 띄어쓰기 연습
+		//
+		// 그냥 이론적 연습
+		for (int k27_i = 0; k27_i < 10; k27_i++) { // 0 ~ 9 까지 for문
+			for (int k27_j = 0; k27_j < k27_i; k27_j++) { // 0 ~ k27_i 까지 for문
+				System.out.printf(" "); // k27_i 값 만큼 띄어쓰기를 출력
+			}
+			System.out.printf("%d\n", k27_i); // k27_i 출력 + 엔터
+		}
+	}
+}
